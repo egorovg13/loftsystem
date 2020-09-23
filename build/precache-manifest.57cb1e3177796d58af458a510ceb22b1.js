7def2b8301ef476663b9c111adb22642f918602d
Grisha Egorov <egorovg13@gmail.com>
2020-09-23 20:41:56 +0300

    rebuild

---
+++ build/precache-manifest.57cb1e3177796d58af458a510ceb22b1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab5609cfd6daded21585a278b4cc6df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74c3e1cc80d633b63478",
+    "url": "/static/js/2.df5813cb.chunk.js"
+  },
+  {
+    "revision": "5afa61899d6aa092bc37",
+    "url": "/static/js/main.69cd9a9d.chunk.js"
+  },
+  {
+    "revision": "baefef36d43e832e04d7",
+    "url": "/static/js/runtime~main.f331c780.js"
+  }
+]);
